19d44fc6ce560711c16a2879446efc1eebecee99
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-04-14 21:21:16 -0600

    auth.js fix

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthContextProvider } from "./contexts/AuthContext.js";
+ 
+export default function Layout({ children }) {
+  return <AuthContextProvider>{children}</AuthContextProvider>;
+}
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- app/page.js
+++ app/page.js
@@ -1,5 +1,22 @@
 import Image from "next/image";
 
+// Import the useUserAuth hook
+import { useUserAuth } from "../contexts/AuthContext";
+ 
+// Use the useUserAuth hook to get the user object and the login and logout functions
+const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+ 
+// Sign in to Firebase with GitHub authentication
+await gitHubSignIn();
+ 
+// Sign out of Firebase
+await firebaseSignOut();
+ 
+// Display some of the user's information
+<p>
+  Welcome, {user.displayName} ({user.email})
+</p>;
+
 export default function Home() {
   return (
     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
