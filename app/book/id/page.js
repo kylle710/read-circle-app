f8b3e12ce0b13bd3eb4c85bf4885ecdc2f43cd3c
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-04-14 21:52:15 -0600

    updatebookPage

---
+++ app/book/id/page.js
@@ -0,0 +1,20 @@
+"use client";
+import { useParams } from "next/navigation";
+import { useUserAuth } from "../../../context/AuthContext"; // Note the three sets of dots!
+
+export default function BookDetailPage() {
+  const { id } = useParams();
+  const { user } = useUserAuth();
+
+  return (
+    <div className="p-8">
+      <h1>Book Details</h1>
+      <p>Viewing book with ID: {id}</p>
+      {user ? (
+        <p>You are logged in as {user.displayName}</p>
+      ) : (
+        <p>Please log in to see more details.</p>
+      )}
+    </div>
+  );
+}
